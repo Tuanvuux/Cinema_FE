1865e02313f727f24982158d356e77fc0f9e1b7b
Tuan Vu <Nguyentuanvu1410@gmail.com>
2025-05-06 21:36:15 +0700

    ok

--- src/routes/ProtectAdminRoute.jsx
+++ src/routes/ProtectAdminRoute.jsx
@@ -1,12 +1,14 @@
-// src/routes/AdminRoute.jsx
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
 const ProtectAdminRoute = ({ children }) => {
   const { user, loading } = useAuth();
+  const location = useLocation();
+
   if (loading) return <div>Loading...</div>;
+
   if (!user || user.role !== "ADMIN") {
-    return <Navigate to="/login" />;
+    return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
   return children;
